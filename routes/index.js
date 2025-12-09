aaaf27ca3fa60c5f649ecb3eed8519ecfb0bdf48
Yana Wahyuna <yana.wahyuna@epson.co.id>
2025-12-09 11:34:39 +0700

    Secret

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+router.all('/secret', (req, res, next) => {
+    console.log('Accessing the secret section ...');
+
+    // Simple authentication check: Look for a specific Authorization header
+    if (req.headers.authorization === 'Bearer my-secret-token') {
+        console.log('Authentication successful!');
+        next(); // pass control to the next handler
+    } else {
+        console.log('Authentication failed!');
+        res.status(401).send('Unauthorized: Access is denied.');
+    }
+});
+
+// This GET handler for '/secret' will only run if the `router.all` middleware above calls next()
+router.get('/secret', (req, res, next) => {
+    res.send('Welcome to the secret area. You have been authenticated!');
+});
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.send('Hello World!');
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    res.send('Got a GET request');
 });
 
 router.put('/user', (req, res) => {
-    res.send('Got a PUT request at /user');
+    res.send('Got a PUT request');
 });
 
 router.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user');
+    res.send('Got a DELETE request');
 });
 
 module.exports = router;
